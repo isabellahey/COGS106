c8dba81e2c444e80c5684f48913057b05240b243
isabellahey <122940850+isabellahey@users.noreply.github.com>
2023-03-23 22:04:13 -0700

    update

--- Metropolis.m
+++ Metropolis.m
@@ -19,15 +19,15 @@ classdef Metropolis
             obj.samples = [];
         end
         
-        function obj = adapt(obj, blockLengths)
+        function self = adapt(self, blockLengths)
             nBlocks = length(blockLengths);
             acceptRates = zeros(1, nBlocks);
             for i = 1:nBlocks
                 acceptances = zeros(1, blocklengths(i));
                 for j = 1:blockLengths(i)
-                    proposal = obj.state + obj.sigma * randn();
-                    if obj.accept(proposal);
-                    obj.state = proposal;
+                    proposal = self.state + self.sigma * randn();
+                    if self.accept(proposal);
+                    self.state = proposal;
                     acceptances(j) = 1;
                 end
             end 
@@ -37,36 +37,36 @@ classdef Metropolis
             targetAcceptRate = 0.4;
             adjFactor = meanAcceptRate / targetAcceptRate;
             if adjFactor > 1.2
-                obj.sigma = obj.sigma / adjFactor;
+                self.sigma = self.sigma / adjFactor;
             elseif adjFactor < 0.8
-                obj.sigma = obj.sigma * adjFactor;
+                self.sigma = self.sigma * adjFactor;
             end
         end
         
-        function yesno = accept(obj, proposal)
-            logRatio = obj.logTarget(proposal) - obj.logTarget(obj.state);
+        function yesno = accept(self, proposal)
+            logRatio = self.logTarget(proposal) - self.logTarget(obj.state);
             if log(rand()) < logRatio
                 yesno = true;
-                obj.accProb = obj.accProb + 1;
+                self.accProb = self.accProb + 1;
             else
                 yesno = flase;
             end
         end
         
-        function obj = sample(obj, n)
-            obj.samples = zeros(1, n);
+        function self = sample(self, n)
+            self.samples = zeros(1, n);
             for i = 1:n
-                proposal = normrnd(obj.state, obj.sigma);
-                if obj.accept(proposal)
-                obj.state = proposal;
+                proposal = normrnd(self.state, self.sigma);
+                if self.accept(proposal)
+                self.state = proposal;
                 end
-                obj.samples(i) = obj.state;
+                self.samples(i) = self.state;
             end
         end
         
         function summ = summary(obj)
-            meanVal = mean(obj.samples);
-            ci = prctile(obj.samples, [2.5, 97.5]);
+            meanVal = mean(self.samples);
+            ci = prctile(self.samples, [2.5, 97.5]);
             summ.mean = meanVal;
             summ.ci = ci;
             summ.c25 = ci(1);
